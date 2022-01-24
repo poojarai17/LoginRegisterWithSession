9b0661d09d27255768e1b0afd4c97a7c61068d2d
unknown <kr.pooja.rai@gmail.com>
2022-01-24 16:45:05 +0530

    Project files

---
+++ src/main/java/com/demo/spring/login/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.demo.spring.login.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.demo.spring.login.models.ERole;
+import com.demo.spring.login.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
